29eb5c0e4d84c527894f27a78aa09c0b42d8bb80
kazuya tamaki <n21015@std.it-college.ac.jp>
2021-09-13 07:15:22 +0000

    APIkadai2

---
+++ api.py
@@ -0,0 +1,18 @@
+import requests as req
+
+r_url = "https://zipcloud.ibsnet.co.jp/api/search"
+
+code = str(input("7桁の数字を入力:  "))
+input_code = f"{r_url}?zipcode={code}"
+
+url = req.get(input_code).json()["results"]
+
+post1 = url[0]["address1"]
+post2 = url[0]["address2"]
+post3 = url[0]["address3"]
+
+print("検索結果を表示します")
+final = f"（{post1}の{post2}{post3}でした）"
+
+print(final)
+# print(post1, post2, post3)
